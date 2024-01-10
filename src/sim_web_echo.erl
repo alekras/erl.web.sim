e912df3156aef1cbe054f7b70b214947e9c76d84
alekras <krasnop@bellsouth.net>
2024-01-10 18:55:18 -0500

    Fix bugs after SIM testing.

--- src/sim_web_echo.erl
+++ src/sim_web_echo.erl
@@ -7,26 +7,33 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([callback/1, pingCallback/1, start/0, stop/0, loop/1]).
+-export([callback/2, start/0, stop/0, loop/1]).
 -record(state, {connection::pid()}).
 
 start() ->
 	R = application:ensure_started(mqtt_client),
-	lager:debug("MQTT client application started with:~p~n", [R]),
+	lager:debug("MQTT client application (echo) started with:~p~n", [R]),
 	
 	Port = application:get_env(sim_web, mqtt_port, 8880),
 	Host = application:get_env(sim_web, mqtt_host, "localhost"),
 	ConnType = application:get_env(sim_web, mqtt_conn_type, web_socket),
-	ConnectionPid = mqtt_client:connect(
+	ConnectionPid = mqtt_client:create(echo),
+	ok = mqtt_client:connect(
 		echo, 
-		#connect{client_id = "echo", user_name = "echo", password = <<"echo">>, keep_alive = 6000000, version = '3.1'}, 
-		Host, 
-		Port, 
-		[{conn_type, ConnType}]
+		#connect{
+			client_id = "echo",
+			user_name = "echo",
+			password = <<"echo">>,
+			host = Host,
+			port = Port,
+			conn_type = ConnType,
+			keep_alive = 6000000,
+			version = '3.1'}, 
+		{?MODULE, callback}
 	),
 	lager:debug("Echo >>> connection established. Host:~p Port:~p Type:~p PID:~p~n", [Host, Port, ConnType, ConnectionPid]),
 
-	mqtt_client:subscribe(ConnectionPid, [{"/echo/+", 2, {?MODULE, callback}}]),
+	mqtt_client:subscribe(ConnectionPid, [{"/echo/+", 2}]),
 
 	Pid = spawn_link(?MODULE, loop, [#state{connection = ConnectionPid}]),
 	register(echo_srvs, Pid),
@@ -52,21 +59,20 @@ loop(#state{connection = Conn} = State) ->
 		120000 ->
 			case mqtt_client:status(Conn) of
 				disconnected -> 
-					lager:debug("Echo >>> terminate: connection PID:~p Status:~p~n", [Conn, mqtt_client:status(Conn)]),
+					lager:debug("Echo >>> terminated: connection PID:~p Status:~p~n", [Conn, mqtt_client:status(Conn)]),
 					unregister(echo_srvs);
 				Status ->
-					mqtt_client:pingreq(Conn, {?MODULE, pingCallback}),
+					mqtt_client:pingreq(Conn),
 					lager:debug("Echo >>> Connection status:~p~n",[Status]),
 					loop(State)
 			end
 	end.
 
-pingCallback(A) ->
-	lager:debug("Ping:~p~n",[A]).
-
-callback({_Q, #publish{topic= Topic, qos=_QoS, dup=_Dup, payload= Msg}} = _Arg) ->
-	lager:debug("Echo >>> got message: ~p~n", [_Arg]),
+callback(onReceive, {_, #publish{topic= Topic, qos=_QoS, dup=_Dup, payload= Msg}} = Arg) ->
+	lager:debug("Echo >>> got message: ~p; ~p~n", [onReceive, Arg]),
 	Segments = string:split(Topic, "/", all),
 	NewTopic = "/" ++ lists:nth(3, Segments) ++ "/" ++ lists:nth(2, Segments),
 	lager:debug("Echo >>> send message to topic: ~p~n", [NewTopic]),
-	echo_srvs ! {send, NewTopic, Msg}.
+	echo_srvs ! {send, NewTopic, Msg};
+callback(Event, Arg) ->
+	lager:debug("Echo >>> got message: ~p; ~p~n", [Event, Arg]).
